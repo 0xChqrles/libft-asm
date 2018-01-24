2568dd853b41d1c83d000aed6acbf5a27fd1004c
Charles LANIER <clanier@e2r7p10.42.fr>
2018-01-25 00:46:22 +0100

    the main is alsmost done

--- srcs/ft_cat.s
+++ srcs/ft_cat.s
@@ -11,8 +11,8 @@ _ft_cat:
 	push rbx
 	mov rbx, rdi
 	push rbp
-	mov rsp, rbp
-	sub rsp, BUFF_SIZE
+	mov rbp, rsp
+	sub rbp, BUFF_SIZE
 	cmp rdi, 0
 	jb exit
 
@@ -24,7 +24,7 @@ read:
 	syscall
 	jc exit
 	cmp rax, 0
-	jbe exit
+	je exit
 
 write:
 	mov rdx, rax
@@ -38,7 +38,56 @@ write:
 	jmp read
 
 exit:
-	add rsp, BUFF_SIZE
+	add rbp, BUFF_SIZE
 	pop rbp
 	pop rbx
 	ret
+
+;section .data
+;
+;buff_size   equ     4096
+;
+;%define STDOUT_FILENO 1
+;%define SYS_READ 0x2000003
+;%define SYS_WRITE 0x2000004
+;
+;section .text
+;global _ft_cat
+;
+;; void (int fd)
+;;           |
+;;           v
+;;          rdi
+;
+;_ft_cat:
+;    push    rbx
+;    mov     rbx,    rdi             ; saving fd in rbx (non volatile)
+;    push    rbp
+;    mov     rbp,    rsp             ; allocating char[buff_size] on the stack
+;    sub     rbp,    buff_size       ; rbp points to the buffer (non volatile)
+;
+;read:
+;    mov     rax,    SYS_READ        ; read(
+;    mov     rdi,    rbx             ;   fd,
+;    mov     rsi,    rbp             ;   buff,
+;    mov     rdx,    buff_size       ;   buff_size)
+;    syscall
+;    jc      end
+;    cmp     rax,    0               ; read failed
+;    je      end
+;
+;write:
+;    mov     rdx,    rax             ; write(
+;    mov     rax,    SYS_WRITE       ;   STDOUT_FILENO,
+;    mov     rdi,    STDOUT_FILENO   ;   buff,
+;    mov     rsi,    rbp             ;   size_read)
+;    syscall
+;    jc      end
+;    cmp     rax,    0               ; write failed
+;    je      end
+;    jmp     read                    ; read again
+;
+;end:
+;    pop rbp
+;    pop rbx
+;    ret
---
+++ test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void	ft_cat(int fd);
+
+int main() {
+	ft_cat(42000);
+	printf("hey\n");
+	return (0);
+}
